009886dbae45b7fac00bcc6994be7657a6cbe911
lampitosgames <dantimko1@gmail.com>
2019-05-31 16:31:00 -0400

    Wrote some more tests

--- bot/user-inventory.js
+++ bot/user-inventory.js
@@ -2,15 +2,23 @@ const invTypes = require('./inv-types.js');
 
 const userMap = {};
 
-const getUserInventory = (_userID) => {
-  if (_userID in Object.keys(userMap)) {
-    return userMap[_userID];
+const getUser = (_userID) => {
+  if (!(_userID in Object.keys(userMap))) {
+    userMap[_userID] = {
+      id: _userID,
+      modus: invTypes.ARRAY,
+      inv: [],
+    };
   }
-  return {
-    userID: _userID,
-    modus: invTypes.ARRAY,
-    inv: [],
-  };
+  return userMap[_userID];
 };
 
-module.exports = { getUserInventory };
+const addCard = (_userID, _card) => {
+  if (!(_userID in Object.keys(userMap))) {
+    getUser(_userID);
+  }
+  userMap[_userID].inv.push(_card);
+  return userMap[_userID];
+};
+
+module.exports = { getUser, addCard };
